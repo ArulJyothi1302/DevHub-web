c52de7d19a7e7e0903bda8fd61d0fadd338fbcab
Arul <hariarul2002@gmail.com>
2025-01-09 19:56:52 +0530

    vite + tailwind + daisy ui

---
+++ DevTinderUi/src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from "./Components/navbar";
+import "./index.css";
+function App() {
+  return (
+    <>
+      <Navbar />
+      <h1 className="text-3xl text-white">Hello World</h1>
+      <h1 className="">Welcome</h1>
+    </>
+  );
+}
+
+export default App;
