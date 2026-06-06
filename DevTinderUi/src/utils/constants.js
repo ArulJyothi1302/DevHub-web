c10c849223104cd10c70e56b9dfc492ccceb8878
Arul <hariarul2002@gmail.com>
2026-06-06 21:23:44 +0530

    env

--- DevTinderUi/src/utils/constants.js
+++ DevTinderUi/src/utils/constants.js
@@ -1,2 +1,4 @@
 export const BASE_URL =
-  location.hostname === "localhost" ? "http://localhost:7777" : "https://devtinder-dnhz.onrender.com";
+  location.hostname === "localhost"
+    ? "http://localhost:7777"
+    : import.meta.env.VITE_API_URL;
